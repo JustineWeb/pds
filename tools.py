7c85a2e47654687a7f1e6cf05bd275eeed5f0662
Justine Weber <justine.weber@epfl.ch>
2019-05-23 14:56:10 +0200

    End session. (Added plots).

--- tools.py
+++ tools.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plt
+
 def return_params_mp3_wav(string, DATA_DIRECTORY, WAV_DIRECTORY) :
     # return : directory of files, pattern for find_files_function, file_type
     if string == "mp3" :
@@ -7,3 +9,16 @@ def return_params_mp3_wav(string, DATA_DIRECTORY, WAV_DIRECTORY) :
     else :
         print("Argument should be either \"mp3\" or \"wav\".")
         return
+
+def plot_auc_loss(train_loss_results, train_auc_results) :
+    # modify this : the epoch axis not not integers which doesn't make sense
+    fig, axes = plt.subplots(2, sharex=True, figsize=(12, 8))
+    fig.suptitle('Training Metrics')
+
+    axes[0].set_ylabel("Loss", fontsize=14)
+    axes[0].plot(train_loss_results)
+
+    axes[1].set_ylabel("AUC score", fontsize=14)
+    axes[1].set_xlabel("Epoch", fontsize=14)
+    axes[1].plot(train_auc_results)
+    plt.show()
